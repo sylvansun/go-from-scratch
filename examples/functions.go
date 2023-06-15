439f88579dbe8f252f9794ce5d1f8bb72f5f58de
sylvansun <sun.sylvan@outlook.com>
2023-06-15 17:44:11 +0800

    feat: basic components

---
+++ examples/functions.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func plus(a int, b int) int {
+
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+// example of a variadic function
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+
+	a, b := vals()
+	fmt.Println(a, b)
+
+	_, c := vals()
+	fmt.Println(c)
+
+	sum(1, 2)
+	sum(1, 2, 3)
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
+	fmt.Println(nums)
+}
