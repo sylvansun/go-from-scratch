81ac9e28827460ec83da44df88683f4e672006f4
sylvansun <sun.sylvan@outlook.com>
2023-06-19 12:00:22 +0800

    feat: generics and goroutines

---
+++ examples/intermediate/goroutines.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func add(num *int) {
+	for i := 0; i < 100000; i++ {
+		*num += 1
+	}
+}
+
+func main() {
+
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+	a := 0
+	go add(&a)
+	go add(&a)
+	fmt.Println(a)
+	time.Sleep(time.Second)
+	fmt.Println(a)
+}
