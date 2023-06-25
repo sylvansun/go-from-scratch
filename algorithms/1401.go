05ad7adb6acb16e42c461d9639a890ba81711e79
sylvansun <sun.sylvan@outlook.com>
2023-06-25 15:49:34 +0800

    bug: unable to solve corner cases

---
+++ algorithms/1401.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+// "fmt"
+
+func square_distance(x1 float64, y1 float64, x2 float64, y2 float64) float64 {
+	return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
+}
+
+func checkOverlap(radius int, xCenter int, yCenter int, x1 int, y1 int, x2 int, y2 int) bool {
+	ret := false
+	square_radius := float64(radius) * float64(radius)
+	//enumerate: circle center in rectangle
+	if xCenter <= x2 && xCenter >= x1 && yCenter <= y2 && yCenter >= y1 {
+		ret = true
+	}
+	//enumerate: four corners in circle
+	if square_distance(float64(x1), float64(y1), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x1), float64(y2), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x2), float64(y1), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x2), float64(y2), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+
+	//enumerate: four midpoints in circle
+	if square_distance(float64(x1)/2+float64(x2)/2, float64(y1), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x1)/2+float64(x2)/2, float64(y2), float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x1), float64(y1)/2+float64(y2)/2, float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	if square_distance(float64(x2), float64(y1)/2+float64(y2)/2, float64(xCenter), float64(yCenter)) <= square_radius {
+		ret = true
+	}
+	return ret
+}
+
+func main() {
+	var x = 1.2
+	var y = 1
+	fmt.Println(x < float64(y))
+}
