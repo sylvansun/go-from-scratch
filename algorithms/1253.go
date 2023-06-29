1d658ffdbdd6507c1c42674699208b2ae5e75842
sylvansun <sun.sylvan@outlook.com>
2023-06-29 20:36:03 +0800

    feat: lc1253

---
+++ algorithms/1253.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	// "reflect"
+)
+
+func reconstructMatrix(upper int, lower int, colsum []int) [][]int {
+	ret := make([][]int, 2)
+	for i := 0; i < 2; i++ {
+		ret[i] = make([]int, len(colsum))
+	}
+	unit_indices := make([]int, 0, len(colsum))
+	for index, value := range colsum {
+		switch value {
+		case 2:
+			ret[0][index], ret[1][index] = 1, 1
+			upper -= 1
+			lower -= 1
+			if upper < 0 || lower < 0 {
+				return make([][]int, 0)
+			}
+		case 1:
+			unit_indices = append(unit_indices, index)
+		}
+	}
+	if upper+lower != len(unit_indices) {
+		return make([][]int, 0)
+	} else {
+		for i := 0; i < len(unit_indices); i++ {
+			if upper > 0 {
+				ret[0][unit_indices[i]] = 1
+				upper -= 1
+			} else {
+				ret[1][unit_indices[i]] = 1
+			}
+		}
+	}
+	return ret
+}
+
+func main() {
+	upper := 5
+	lower := 5
+	colsum := []int{2, 1, 2, 0, 1, 0, 1, 2, 0, 1}
+	// fmt.Println(reflect.TypeOf(colsum))
+	// fmt.Println(len(colsum))
+	ret := reconstructMatrix(upper, lower, colsum)
+	fmt.Println(ret)
+}
