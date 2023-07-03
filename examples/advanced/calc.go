4efbe5f9bd141813eef171bcf2ba450aa6b55226
sylvansun <sun.sylvan@outlook.com>
2023-07-03 15:29:05 +0800

    feat: unit test

---
+++ examples/advanced/calc.go
@@ -0,0 +1,13 @@
+package main
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Mul(a int, b int) int {
+	return a * b
+}
+
+func Sub(a int, b int) int {
+	return a - b
+}
---
+++ examples/advanced/calc_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+/*
+usage: under pwd, run command
+go test -v -cover calc_test.go calc.go
+where -v shows verbose output
+and -cover illustrates the percentage of codes tested
+*/
+func TestAdd(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	}
+
+	if ans := Add(-10, -20); ans != -30 {
+		t.Errorf("-10 + -20 expected be -30, but %d got", ans)
+	}
+}
+
+func TestMul(t *testing.T) {
+	t.Run("pos", func(t *testing.T) {
+		if Mul(2, 3) != 6 {
+			t.Fatal("fail")
+		}
+
+	})
+	t.Run("neg", func(t *testing.T) {
+		if Mul(2, -3) != -6 {
+			t.Fatal("fail")
+		}
+	})
+}
