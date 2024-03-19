d9395d1b8a3c10e4fab64d62b906eac875e57360
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2024-03-19 16:50:07 +0800

    feat: append test cases

--- examples/beginner/slices_test.go
+++ examples/beginner/slices_test.go
@@ -18,3 +18,25 @@ func TestAvgFloat64(t *testing.T) {
 		})
 	})
 }
+
+func TestAppend(t *testing.T) {
+	Convey("Append", t, func() {
+
+		Convey("single test case", func() {
+			arrs := [][]int{{1, 2}}
+			arr := []int{3, 4, 5}
+			fmt.Println(arrs, arr)
+			arrs = append(arrs, arr)
+			arr[0] = 100
+			fmt.Println(arrs, arr)
+		})
+		Convey("another test case", func() {
+			arr := []int{3, 4, 5}
+			a := 6
+			fmt.Println(arr, a)
+			arr = append(arr, a)
+			a = 100
+			fmt.Println(arr, a)
+		})
+	})
+}
